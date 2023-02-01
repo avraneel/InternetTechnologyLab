64a0461c7679d4d32befee947b6644c5c380424c
avraneel <avraneelpal22@gmail.com>
2023-02-01 20:11:49 +0530

    added code

---
+++ Assignment 2/code/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const multer = require('multer');
+const app = express();
+const port = 5500;
+
+app.use(express.urlencoded)
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}!`);
+});
+
+const fileStorageEngine = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './img')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.filename + "_" + Date.now() + ".jpg")
+    }
+});
+
+const upload = multer({storage: fileStorageEngine});
+
+app.get('/', (req, res) => {
+    res.send("Welcome to server");
+});
+
+app.post('/index.js', upload.single('img'), (req, res) => {
+    console.log(req.body)
+    res.send('POST request to homepage');   
+});
+
