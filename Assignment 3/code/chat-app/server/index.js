c7f7de7ad3a6da2bfc98e4b002db54677973e258
avraneel <avraneelpal22@gmail.com>
2023-02-14 15:38:24 +0530

    worked on project

---
+++ Assignment 3/code/chat-app/server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server);
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('chat message', (msg) => {
+    console.log(msg);
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    io.emit('disconnect message', msg)
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Listening on *:3000');
+});
